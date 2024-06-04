3dc45bb5776db59634ba92da4dca99d35088b202
Dhanish18 <dhanishmarudhu@gmail.com>
2024-06-04 12:44:15 +0530

    route structure

--- main.js
+++ main.js
@@ -2,7 +2,23 @@ const express = require("express");
 const app = express();
 const PORT = 8000;
 
-app.get("/", (req, res) => {});
+app.get("/", (req, res) => {
+  return res.json({ msg: "hellooo" });
+});
+
+//CRUD functionality for movies
+
+//R - for reading
+app.get("/movies", () => {});
+
+//C - for creating
+app.post("/movies", () => {});
+
+//U - for update
+app.patch("/movies", () => {});
+
+//D - for delete
+app.delete("/movies", () => {});
 
 app.listen(PORT, () => {
   console.log(`app is running on the port ${PORT}`);
